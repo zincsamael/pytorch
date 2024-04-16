d9c2410eeed12276d2fa840e2f2d7475bc725798
ydwu4 <ydwu2014@gmail.com>
2024-04-16 13:32:13 -0700

    Support aot_export torchbind op
    ghstack-source-id: 8aecd830befcc3cca1533361a6f2c4647e1ee996
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/123370

--- torch/_ops.py
+++ torch/_ops.py
@@ -782,6 +782,9 @@ class OpOverload(OperatorBase):
 class TorchBindOpOverload(OpOverload):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        from torch._higher_order_ops.effects import _EffectType, _register_effectful_op
+
+        _register_effectful_op(self, _EffectType.ORDERED)
 
     def _fallthrough_keys(self) -> List[DispatchKey]:
         # TODO: we should be calling the fallback for these, but a fallthrough is almost close
@@ -792,6 +795,7 @@ class TorchBindOpOverload(OpOverload):
             DispatchKey.AutogradCUDA,
             DispatchKey.ADInplaceOrView,
             DispatchKey.PythonTLSSnapshot,
+            DispatchKey.PythonDispatcher,
         ]
 
         def _may_use_fallthrough_instead_of_fallback(key: DispatchKey):
