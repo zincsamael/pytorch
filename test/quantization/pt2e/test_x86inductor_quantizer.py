92b3d336f402665bee99075e1deab7c614466ebc
leslie-fang-intel <leslie.fang@intel.com>
2024-04-17 13:27:34 +0800

    Test OSS PreCI for X86InductorQuantizer
    ghstack-source-id: 4b1751d135107ca9a195e24ae6fe4ed1742762b8
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/123923

--- test/quantization/pt2e/test_x86inductor_quantizer.py
+++ test/quantization/pt2e/test_x86inductor_quantizer.py
@@ -1642,3 +1642,4 @@ class TestQuantizePT2EX86Inductor(X86InductorQuantTestCase):
                     node_occurrence,
                     node_list,
                 )
+                self.assertTrue(False)
