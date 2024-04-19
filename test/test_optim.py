d1d9430d9209ab585c685d2802516d1c1f9d2e08
Michael Lazos <mlazos@meta.com>
2024-04-18 22:32:05 -0700

    Fixes for 123400
    ghstack-source-id: e8c387c8d7a403759eb0ba2a471a2f6de11786f5
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/123406

--- test/test_optim.py
+++ test/test_optim.py
@@ -1588,7 +1588,11 @@ class TestOptimRenewed(TestCase):
 
             # Make some state
             for _ in range(3):
-                optimizer.step(closure)
+                if optim_info.step_requires_closure:
+                    optimizer.step(closure)
+                else:
+                    closure()
+                    optimizer.step()
 
             self.assertEqual(getPublicAttrs(optimizer), getPublicAttrs(deepcopy(optimizer)))
 
