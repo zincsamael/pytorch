fb80fee1ff4cbf732aed8eea96a8e6b1e42b860b
David Berard <dberard@fb.com>
2024-04-24 14:10:51 -0700

    [WIP] Support NT constructed in a graph and returned
    ghstack-source-id: b91f5f96c52fcf4d72a15f99172f966f92048d32
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/124803

--- test/test_nestedtensor.py
+++ test/test_nestedtensor.py
@@ -3997,6 +3997,26 @@ class TestNestedTensorSubclass(TestCase):
         nt1_t, nt2_t, nt3_t, nt4_t = (x.transpose(1, 2) for x in (nt1, nt2, nt3, nt4))
         check_size(nt1_t, nt2_t, nt3_t, nt4_t)
 
+    @skipCUDAIf(not SM70OrLater, "GPU capability is < SM70")
+    def test_return_nt_constructed_in_graph(self, device):
+        def fn(values, offsets):
+            nt = torch.nested.nested_tensor_from_jagged(values, offsets)
+            nt = nt.cos()
+            return nt
+
+        values = torch.rand((10, 8), device=device)
+        offsets = torch.tensor([0, 1, 3, 6, 10], device=device)
+
+        fn_c = torch.compile(fn, fullgraph=True)
+
+        ref = fn(values, offsets)
+        res = fn_c(values, offsets)
+        self.assertEqual(ref, res)
+
+    def test_nested_int_concrete_int(self):
+        j1 = torch._C._get_nested_int(1, 1)
+        self.assertTrue(torch.fx.experimental.symbolic_shapes.is_concrete_int(j1))
+
     # Doesn't work until we have real views
     @xfailIfTorchDynamo
     # Note 1: Math fallback doesn't work with bfloat16 on CUDA
