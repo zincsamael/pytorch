ff5ad141817157a4ba2b5d92473cd3435c346f4b
James Wu <jjwu@meta.com>
2024-05-04 17:08:51 -0700

    AOTAutogradCache first steps
    ghstack-source-id: 9295a6f5fdb6fe8ac11704be66952677df3b6963
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/125541

--- test/dynamo/test_aot_autograd_cache.py
+++ test/dynamo/test_aot_autograd_cache.py
@@ -1,18 +1,109 @@
 # Owner(s): ["module: dynamo"]
 
+import os
+
 import torch
 import torch._dynamo
 import torch._dynamo.test_case
-
 import torch._functorch._aot_autograd
+from torch._dynamo.utils import counters
+from torch._functorch import config
 from torch._functorch._aot_autograd.autograd_cache import (
+    AOTAutogradCache,
     autograd_cache_hash,
     BypassAOTAutogradCache,
+    check_cacheable,
 )
 from torch._functorch._aot_autograd.schemas import AOTConfig
 
 
+torch._inductor.config.fx_graph_cache = True
+
+
+class AOTAutogradCacheTests(torch._dynamo.test_case.TestCase):
+    def setUp(self):
+        """
+        Reset all counters and caches before each unit test
+        """
+        super().setUp()
+        counters.clear()
+        self._clear_all_caches()
+
+    def _clear_all_caches(self):
+        """
+        Clear every cache, including AOTAutgradCache and FXCache
+        """
+        torch._inductor.codecache.FxGraphCache.clear()
+        AOTAutogradCache.clear()
+        self._clear_extra_caches()
+
+    def _clear_extra_caches(self):
+        """
+        Clear unrelated caches, like dynamo and PyCodeCache
+        """
+        torch._dynamo.reset()
+        for m in torch._inductor.codecache.PyCodeCache.cache.values():
+            os.remove(m.__file__)
+        torch._inductor.codecache.PyCodeCache.cache_clear()
+
+    @config.patch({"enable_aot_autograd_cache": True})
+    def test_basic(self):
+        """
+        Verify the interactions between FXGraphCache and AOTAutogradCache.
+        """
+
+        def fn(x, y):
+            return (x * 2, y @ y)
+
+        a = torch.rand(25)
+        b = torch.rand(5, 5)
+
+        compiled_fn = torch.compile(fn, backend="inductor")
+
+        # A first call should miss in the cache.
+        self.assertEqual(fn(a, b), compiled_fn(a, b))
+        self.assertEqual(counters["aot_autograd"]["autograd_cache_miss"], 1)
+        self.assertEqual(counters["aot_autograd"]["autograd_cache_hit"], 0)
+
+        # A second call should hit. (First reset so in-memory guards
+        # don't prevent compilation).
+        self._clear_extra_caches()
+        self.assertEqual(fn(a, b), compiled_fn(a, b))
+
+        self.assertEqual(counters["aot_autograd"]["autograd_cache_miss"], 1)
+        self.assertEqual(counters["aot_autograd"]["autograd_cache_hit"], 1)
+
+    @config.patch({"enable_aot_autograd_cache": True})
+    def test_autograd_function(self):
+        """
+        Autograd functions are not supported in the cache yet
+        """
+
+        def fn(x, y):
+            return (x * 2, y @ y)
+
+        a = torch.rand(25, requires_grad=True)
+        b = torch.rand(5, 5, requires_grad=True)
+
+        compiled_fn = torch.compile(fn, backend="inductor")
+
+        # A first call should miss in the cache.
+        self.assertEqual(fn(a, b), compiled_fn(a, b))
+        self.assertEqual(counters["aot_autograd"]["autograd_cache_miss"], 1)
+        self.assertEqual(counters["aot_autograd"]["autograd_cache_hit"], 0)
+
+        # Second call should hit FXGraphCache, miss AOTAutogradCache
+        self._clear_extra_caches()
+        self.assertEqual(fn(a, b), compiled_fn(a, b))
+        self.assertEqual(counters["aot_autograd"]["autograd_cache_miss"], 2)
+        self.assertEqual(counters["aot_autograd"]["autograd_cache_hit"], 0)
+
+
 class AOTAutogradCachePicklerTests(torch._dynamo.test_case.TestCase):
+    """
+    Tests for cache key generation
+    """
+
     def default_config(self):
         return AOTConfig(
             fw_compiler=None,
@@ -48,6 +139,7 @@ class AOTAutogradCachePicklerTests(torch._dynamo.test_case.TestCase):
         if inputs is None:
             inputs = [torch.randn(3)]
         _, fx_g = self._get_dynamo_output(f, *inputs)
+        check_cacheable(fx_g)
         return autograd_cache_hash(fx_g, config)
 
     def test_basic_hash_key(self):
