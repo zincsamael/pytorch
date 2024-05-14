ce9e2b70339f46f1d1a84114ce87472f53e0cc20
PaliC <sahancpal@gmail.com>
2024-05-13 18:10:43 -0700

    [Split Build] add option to create a libtorch wheel
    ghstack-source-id: 9255ce39e17ec6fbaea747fae79fe227b5456f71
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/125715

--- setup.py
+++ setup.py
@@ -220,7 +220,14 @@
 #
 #   USE_PRIORITIZED_TEXT_FOR_LD
 #      Uses prioritized text form cmake/prioritized_text.txt for LD
+#
+#   BUILD_LIBTORCH_WHL
+#      Builds libtorch.so and its dependencies as a wheel
+#
+#   BUILD_PYTORCH_USING_LIBTORCH_WHL
+#      Builds pytorch as a wheel using libtorch.so from a seperate wheel [not supported yet]
 
+import os
 import sys
 
 if sys.platform == "win32" and sys.maxsize.bit_length() == 31:
@@ -230,6 +237,19 @@ if sys.platform == "win32" and sys.maxsize.bit_length() == 31:
     sys.exit(-1)
 
 import platform
+
+BUILD_LIBTORCH_WHL = os.getenv("BUILD_LIBTORCH_WHL", "0") == "1"
+BUILD_PYTORCH_USING_LIBTORCH_WHL = False
+
+# set up appropriate env variables
+if BUILD_LIBTORCH_WHL:
+    # Set up environment variables for ONLY building libtorch.so and not libtorch_python.so
+
+    # functorch is not supported without python
+    os.environ["BUILD_FUNCTORCH"] = "OFF"
+    os.environ["BUILD_PYTHONLESS"] = "ON"
+else:
+    os.environ["BUILD_PYTHONLESS"] = "OFF"
 
 python_min_version = (3, 8, 0)
 python_min_version_str = ".".join(map(str, python_min_version))
@@ -335,7 +355,10 @@ cmake_python_include_dir = sysconfig.get_path("include")
 ################################################################################
 # Version, create_version_file, and package_name
 ################################################################################
-package_name = os.getenv("TORCH_PACKAGE_NAME", "torch")
+
+DEFAULT_PACKAGE_NAME = "libtorch" if BUILD_LIBTORCH_WHL else "torch"
+
+package_name = os.getenv("TORCH_PACKAGE_NAME", DEFAULT_PACKAGE_NAME)
 package_type = os.getenv("PACKAGE_TYPE", "wheel")
 version = get_torch_version()
 report(f"Building wheel {package_name}-{version}")
@@ -459,10 +482,12 @@ def build_deps():
     check_submodules()
     check_pydep("yaml", "pyyaml")
 
+    build_python = not BUILD_LIBTORCH_WHL
+
     build_caffe2(
         version=version,
         cmake_python_library=cmake_python_library,
-        build_python=True,
+        build_python=build_python,
         rerun_cmake=RERUN_CMAKE,
         cmake_only=CMAKE_ONLY,
         cmake=cmake,
@@ -719,6 +744,8 @@ class build_ext(setuptools.command.build_ext.build_ext):
             "caffe2.python.caffe2_pybind11_state_gpu",
             "caffe2.python.caffe2_pybind11_state_hip",
         ]
+        if BUILD_LIBTORCH_WHL:
+            caffe2_pybind_exts = []
         i = 0
         while i < len(self.extensions):
             ext = self.extensions[i]
@@ -950,8 +977,13 @@ def configure_extension_build():
 
     main_compile_args = []
     main_libraries = ["torch_python"]
+
     main_link_args = []
     main_sources = ["torch/csrc/stub.c"]
+
+    if BUILD_LIBTORCH_WHL:
+        main_libraries = ["torch"]
+        main_sources = []
 
     if cmake_cache_vars["USE_CUDA"]:
         library_dirs.append(os.path.dirname(cmake_cache_vars["CUDA_CUDA_LIB"]))
@@ -1074,7 +1106,6 @@ def configure_extension_build():
             "default = torch.distributed.elastic.multiprocessing:DefaultLogsSpecs",
         ],
     }
-
     return extensions, cmdclass, packages, entry_points, extra_install_requires
 
 
@@ -1098,6 +1129,32 @@ def print_box(msg):
     for l in lines:
         print("|{}{}|".format(l, " " * (size - len(l))))
     print("-" * (size + 2))
+
+
+def rename_torch_packages(package_list):
+    """
+    Create a dictionary from a list of package names, renaming packages where
+    the top-level package is 'torch' to 'libtorch'.
+
+    Args:
+        package_list (list of str): The list of package names.
+
+    Returns:
+        dict: A dictionary where keys are the package names with 'torch' replaced by 'libtorch',
+              and values are the original package names, only including those where the
+              top-level name is 'torch'.
+    """
+    result = {}
+    for package in package_list:
+        # Split the package name by dots to handle subpackages or modules
+        parts = package.split(".")
+        # Check if the top-level package is 'torch'
+        if parts[0] == "torch":
+            # Replace 'torch' with 'libtorch' in the top-level package name
+            new_key = "libtorch" + package[len("torch") :]
+            result[new_key] = package
+
+    return result
 
 
 def main():
@@ -1187,13 +1244,11 @@ def main():
         "nn/parallel/*.pyi",
         "utils/data/*.pyi",
         "utils/data/datapipes/*.pyi",
-        "lib/*.so*",
-        "lib/*.dylib*",
-        "lib/*.dll",
-        "lib/*.lib",
         "lib/*.pdb",
         "lib/torch_shm_manager",
         "lib/*.h",
+        "lib/libtorch_python*",
+        "lib/*shm*",
         "include/*.h",
         "include/ATen/*.h",
         "include/ATen/cpu/*.h",
@@ -1354,6 +1409,15 @@ def main():
         "utils/model_dump/code.js",
         "utils/model_dump/*.mjs",
     ]
+    if not BUILD_PYTORCH_USING_LIBTORCH_WHL:
+        torch_package_data.extend(
+            [
+                "lib/*.so*",
+                "lib/*.dylib*",
+                "lib/*.dll",
+                "lib/*.lib",
+            ]
+        )
 
     if get_cmake_cache_vars()["BUILD_CAFFE2"]:
         torch_package_data.extend(
@@ -1380,25 +1444,36 @@ def main():
                 "include/tensorpipe/transport/uv/*.h",
             ]
         )
-    if get_cmake_cache_vars()["USE_KINETO"]:
-        torch_package_data.extend(
-            [
-                "include/kineto/*.h",
-            ]
-        )
     torchgen_package_data = [
-        # Recursive glob doesn't work in setup.py,
-        # https://github.com/pypa/setuptools/issues/1806
-        # To make this robust we should replace it with some code that
-        # returns a list of everything under packaged/
-        "packaged/ATen/*",
-        "packaged/ATen/native/*",
-        "packaged/ATen/templates/*",
-        "packaged/autograd/*",
-        "packaged/autograd/templates/*",
+        "packaged/**/*.cpp",
+        "packaged/**/*.h",
+        "packaged/**/*.yaml",
     ]
+
+    if BUILD_LIBTORCH_WHL:
+        modified_packages = []
+        for package in packages:
+            parts = package.split(".")
+            if parts[0] == "torch":
+                modified_packages.append("libtorch" + package[len("torch") :])
+        packages = modified_packages
+        package_dir = {"libtorch": "torch"}
+        torch_package_dir_name = "libtorch"
+        package_data = {"libtorch": torch_package_data}
+        extensions = []
+    else:
+        torch_package_dir_name = "torch"
+        package_dir = {}
+        package_data = {
+            "torch": torch_package_data,
+            "torchgen": torchgen_package_data,
+            "caffe2": [
+                "python/serialized_test/data/operator_test/*.zip",
+            ],
+        }
+
     setup(
-        name=package_name,
+        name="libtorch",
         version=version,
         description=(
             "Tensors and Dynamic neural networks in "
@@ -1412,13 +1487,9 @@ def main():
         entry_points=entry_points,
         install_requires=install_requires,
         extras_require=extras_require,
-        package_data={
-            "torch": torch_package_data,
-            "torchgen": torchgen_package_data,
-            "caffe2": [
-                "python/serialized_test/data/operator_test/*.zip",
-            ],
-        },
+        package_dir=package_dir,
+        package_data=package_data,
+        include_package_data=True,
         url="https://pytorch.org/",
         download_url="https://github.com/pytorch/pytorch/tags",
         author="PyTorch Team",
